bffa9eed950eb222b1d7679dc1deb54813bc15b9
phnaharris <phnanh.harris@gmail.com>
2023-11-14 21:18:34 +0700

    refactor(structure): make this folder cleaner

--- scheduler.sh
+++
@@ -1,2 +0,0 @@
-#!usr/bin/env bash
-echo 'notify-send "your message"' | at 10.20 pm
--- tmux-webrtc-basic.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source /data/repos/phnaharris-machos/scripts/start-tmux.sh
-
-start_tmux /data/repos/beowulf-foundation/webrtc-simple-applications/browser-to-browser
-start_tmux /data/repos/beowulf-foundation/webrtc-simple-applications/browser-webrtcrs-browser
-start_tmux /data/repos/beowulf-foundation/webrtc-simple-applications/multiclient-webrtcrs
-start_tmux /data/repos/beowulf-foundation/webrtc-simple-applications/signaling-server
-start_tmux /data/repos/phnaharris-bwf/media-server-rs
